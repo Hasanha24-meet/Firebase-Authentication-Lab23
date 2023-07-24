b8121866b3545a2d0b743b253f3e010096656d31
Hasanhamdan789 <HasanHamdan789@gmail.com>
2023-07-24 12:15:07 +0300

    .

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -16,18 +16,18 @@ firebaseConfig = {
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/signin', methods=['GET', 'POST'])
-def signin():
+@app.route('/', methods=['GET', 'POST'])
+def signin():  
    error = ""
    if request.method == 'POST':
        email = request.form['email']
        password = request.form['password']
        try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url_for('home'))
+            return redirect(url_for('add_tweet.html'))
        except:
            error = "Authentication failed"
-   return render_template("add_tweet.html")
+   return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -38,10 +38,10 @@ def signup():
        password = request.form['password']
        try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('home'))
+            return redirect(url_for('add_tweet.html'))
        except:
            error = "Authentication failed"
-   return render_template("add_tweet.html")
+   return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
